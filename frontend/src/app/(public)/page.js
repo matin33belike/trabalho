0301ff8888c3dc61fecff80754ee1b5b3588494d
Mateus <moreiradasilvamateus214@gmail.com>
2026-05-28 22:58:55 -0400

    alterações frontend

--- frontend/src/app/(public)/page.js
+++ frontend/src/app/(public)/page.js
@@ -8,27 +8,9 @@ export default function Home() {
 				<h1 className="text-3xl font-bold text-center text-gray-800 dark:text-gray-200 mb-2">
 					Home
 				</h1>
-				<p className="text-center text-gray-600 dark:text-gray-400 mb-6">
+				<h2 className="text-center text-gray-600 dark:text-gray-400 mb-6">
 					Bem-vindo ao gerenciador de tarefas!
-				</p>
-
-				<h2 className="text-xl font-semibold text-gray-700 dark:text-gray-300 mb-4">
-					Suas Tarefas
 				</h2>
-
-				<div className="flex flex-col gap-3">
-					<Button className="w-full bg-green-500 hover:bg-green-600 transition text-white py-2 rounded-lg">
-						Adicionar Tarefa
-					</Button>
-
-					<Button className="w-full bg-blue-500 hover:bg-blue-600 transition text-white py-2 rounded-lg">
-						Editar Tarefa
-					</Button>
-
-					<Button className="w-full bg-red-500 hover:bg-red-600 transition text-white py-2 rounded-lg">
-						Remover Tarefa
-					</Button>
-				</div>
 				<div className="mt-6 text-center">
 					<Link href="/cadastro" className="px-6 text-center text-sm text-gray-600 dark:text-gray-400 hover:underline">
 						Ir para página de cadastro
